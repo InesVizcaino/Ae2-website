0a66b9a7855c1cba17c39af625c8304c6cc2743e
CristinaH <16805070-CristinaH@users.noreply.replit.com>
2023-05-30 14:22:01 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS= [
-{
-  'id':1,
-  'title':"Date Analyst",
-  'location': 'Bengaluru, India',
-  'salary': 'Rs. 1,00,000'
-},
-{
-  'id':2,
-  'title':"Date Scientist",
-  'location': 'Delhi, India',
-  'salary':'Rs. 15,00,000'
-},
-{
-  'id':3,
-  'title':"Frontend Engineer",
-  'location': 'Remote',
-  'salary':'Rs. 1,00,000'
-},
+JOBS = [
   {
-  'id':4,
-  'title':"Backend Engineer",
-  'location': 'San Francisco',
-  'salary':'Rs. $120,000'
+    'id':1,
+    'title':'Data Analyst',
+    'location': 'Veracruz',
+    'salary': ' Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location': 'Guadalajara',
+    'salary': ' Rs. 15,00,000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location': 'Monterrey',
+    'salary': ' Rs. 12,00,000'
+  },
+  {
+    'id':4,
+    'title':'Backend Enginner',
+    'location': 'San Francisco USA',
+    'salary': '$120.000'
   }
 ]
 
 @app.route("/")
 def helloworld():
-  return render_template('home.html',
-                        jobs=JOBS)
+  return render_template('home.html', 
+                         jobs=JOBS, 
+                         company_name='Peace Shop')
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
